8757163fe6e3d88e9e1e12485b4323368caa08eb
Pierre Boudoin <pierre.boudoin@gmail.com>
2022-02-08 13:59:26 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -24,7 +24,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Sliders",
-            dependencies: ["CGExtender", "Shapes", "bez"]),
+            dependencies: ["CGExtender", "SwiftUI-Shapes", "SwiftUI-bez"]),
         .testTarget(
             name: "SlidersTests",
             dependencies: ["Sliders"]),
